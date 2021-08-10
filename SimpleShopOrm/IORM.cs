96518a1ba7a9f9c8a8521fe3ecd8fef666b7d60e
aksschnell <54832919+aksschnell@users.noreply.github.com>
2021-08-10 09:23:38 +0200

    sql/orm tilføjet

---
+++ SimpleShopOrm/IORM.cs
@@ -0,0 +1,17 @@
+﻿using SimpleShodModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShopOrm
+{
+    public interface IORM
+    {
+        public List<Customer> GetCustomers();
+        public Customer GetCustomer(int id);
+        public List<Product> GetProducts();
+        public Product GetProduct(int id);
+    }
+}
---
+++ SimpleShopOrm/ORM_MsSql.cs
@@ -0,0 +1,54 @@
+﻿using SimpleShodModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShopOrm
+{
+    public class ORM_MsSql : IORM
+    {
+
+        private SqlConnection dbConn;
+        private string host;
+        private string username;
+        private string password;
+        private string database;
+
+
+        public ORM_MsSql()
+        {
+            SqlConnectionStringBuilder conString = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = database,
+                UserID = username,
+                Password = password,
+                DataSource = host
+
+            };
+        }
+
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
